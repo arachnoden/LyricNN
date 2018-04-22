1fcd9c47b490835753e3589301815ac9c879c9bf
Den <Den@DESKTOP-15Q7AUP>
2018-04-23 01:18:22 +0300

    fisation 1

--- src/lyricnn/LyricNN.java
+++ src/lyricnn/LyricNN.java
@@ -182,18 +182,24 @@ public class LyricNN extends Application {
      * Обновляє номери слів
      */
     public static void updateNumbers(){
+        int[] nums = new int[10];
         for (int i = 0; i < 10; i++) {
             lblNom[i].setText(""+cbMass[i].getSelectionModel().getSelectedIndex());
+            nums[i]=cbMass[i].getSelectionModel().getSelectedIndex();
         }
-        fillTextAnswer(getAnswer());
+        double[] nums2 = new double[262];
+        for (int i = 0; i < 10; i++) {
+            nums2[nums[i]]=1.0;
+        }
+        fillTextAnswer(getAnswer(nums2));
     }
     
     /**
      * Отримує відповідь нейронної мережі
      * @return 
      */
-    public static int[] getAnswer(){
-        double ans[] = nn1.getAnswer(); // нічого не змінювалося тому, що завдання було старим(останнє введене питаня/завдання), необхідно змінювати завдання.
+    public static int[] getAnswer(double[] question){
+        double ans[] = nn1.getAnswer(question); // нічого не змінювалося тому, що завдання було старим(останнє введене питаня/завдання), необхідно змінювати завдання.
         double biggest[] = new double[10];
         int biggestNumb[] = new int[10];
         for (int i = 0; i < ans.length; i++) {
